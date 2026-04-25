563cbbc07da6d9df8a65edb0f25f3297c58de63f
Kreika-hub <kreikaglobalhub@gmail.com>
2026-04-25 16:51:02 -0400

    fix: iconos pwa exactos generados para webapk android

--- vite.config.js
+++ vite.config.js
@@ -17,19 +17,19 @@ export default defineConfig({
         theme_color: '#1a1a2e',
         icons: [
           {
-            src: '/icons/pwa-sloty.png',
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/icons/pwa-sloty.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/icons/pwa-sloty.png',
+            src: 'maskable-icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
